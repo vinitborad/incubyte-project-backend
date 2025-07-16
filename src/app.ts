4d33d3e197fe9b27c7be56abd684fa213424ceec
Vinit Borad <vinitboradofficial@gmail.com>
2025-07-16 22:55:29 +0530

    chore(api): add cors middleware and update server port to 5000

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,11 @@
 import express from 'express';
 import sweetRoutes from './routes/sweet.routes';
+import cors from 'cors';
 
 const app = express();
+
+app.use(cors());
+
 app.use(express.json());
 
 // Use the sweet routes
--- src/server.ts
+++ src/server.ts
@@ -4,6 +4,6 @@ import connectDB from './config/db';
 // Connect to the database
 connectDB();
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
